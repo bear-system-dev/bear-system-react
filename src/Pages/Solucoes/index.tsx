e189a7aa4edcd5102c9abfad44d5d4a6b7d51723
arten <felipe.arten@gmail.com>
2023-09-27 22:00:25 -0300

    add under construction to missing routes

--- src/Pages/Solucoes/index.tsx
+++ src/Pages/Solucoes/index.tsx
@@ -1,3 +1,31 @@
 export function Solucoes() {
-  return <h1>Hello World</h1>
+  return (
+    <div
+      style={{
+        alignItems: 'center',
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        minHeight: 'calc(100vh - 5rem)',
+      }}
+    >
+      <h1
+        style={{
+          fontSize: '8rem',
+          fontWeight: 800,
+          padding: '4rem',
+        }}
+      >
+        Em Construção
+      </h1>
+      <h2
+        style={{
+          fontSize: '2rem',
+          fontWeight: 600,
+        }}
+      >
+        em breve novidades
+      </h2>
+    </div>
+  )
 }
