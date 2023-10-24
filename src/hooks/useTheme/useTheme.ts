057ecbcde57e088a98a3fabdea2fb230835feaa1
artenlf <felipe.arten@gmail.com>
2023-10-23 22:49:18 -0300

    fix: save user current theme to local storage

--- src/hooks/useTheme/useTheme.ts
+++ src/hooks/useTheme/useTheme.ts
@@ -1,4 +1,4 @@
-import { useContext } from 'react'
+import { useContext, useEffect } from 'react'
 import { ThemeContext } from './useTheme.tsx'
 
 export enum Theme {
@@ -12,5 +12,13 @@ export function useTheme() {
   if (context === undefined) {
     throw new Error('Não é possível usar um theme sem um theme provider!')
   }
+
+  useEffect(() => {
+    const storedTheme = localStorage.getItem('theme')
+    if (storedTheme) {
+      context.setTheme(storedTheme as Theme)
+    }
+  }, [context])
+
   return context
 }
--- src/layout/Header/ThemeSelector/index.tsx
+++ src/layout/Header/ThemeSelector/index.tsx
@@ -5,17 +5,21 @@ import './styles.css'
 export function ThemeSelector() {
   const { theme, setTheme } = useTheme()
 
+  function toogleTheme() {
+    if (theme === Theme.Light) {
+      setTheme(Theme.Dark)
+      localStorage.setItem('theme', Theme.Dark)
+    } else if (theme === Theme.Dark) {
+      setTheme(Theme.Bear)
+      localStorage.setItem('theme', Theme.Bear)
+    } else {
+      setTheme(Theme.Light)
+      localStorage.setItem('theme', Theme.Light)
+    }
+  }
+
   return (
-    <button
-      className="theme-selector"
-      onClick={
-        theme === Theme.Light
-          ? () => setTheme(Theme.Dark)
-          : theme === Theme.Dark
-          ? () => setTheme(Theme.Bear)
-          : () => setTheme(Theme.Light)
-      }
-    >
+    <button className="theme-selector" onClick={toogleTheme}>
       {theme === Theme.Light && <MoonStars weight="thin" />}
       {theme === Theme.Dark && <PawPrint weight="thin" />}
       {theme === Theme.Bear && <Lightbulb weight="thin" />}
