1bac98ee6924237708cf2fdf7e6a45322e94b824
artenlf <felipe.arten@gmail.com>
2023-10-23 23:57:11 -0300

    fix: under construction pages responsive design

--- src/Pages/Carreiras/index.tsx
+++ src/Pages/Carreiras/index.tsx
@@ -1,32 +1,11 @@
+import './styles.css'
+
 export function Carreiras() {
   return (
     <main>
-      <div
-        style={{
-          alignItems: 'center',
-          display: 'flex',
-          flexDirection: 'column',
-          justifyContent: 'center',
-          minHeight: '100vh',
-        }}
-      >
-        <h1
-          style={{
-            fontSize: '8rem',
-            fontWeight: 800,
-            padding: '4rem',
-          }}
-        >
-          Em Construção
-        </h1>
-        <h2
-          style={{
-            fontSize: '2rem',
-            fontWeight: 600,
-          }}
-        >
-          em breve novidades
-        </h2>
+      <div className="under-construction-container">
+        <h1 className="under-construction-title">Em Construção</h1>
+        <h2 className="under-construction-subtitle">em breve novidades</h2>
       </div>
     </main>
   )
